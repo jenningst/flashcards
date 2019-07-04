2daae1a1a3737616412ada7643f0a7c49ab7d9ac
Troy Jennings <troy.c.jennings@gmail.com>
2019-07-04 14:16:34 +0200

    Adds initial unit tests to PackCard
    - Increasing testing coverage across the app
    - Adds unit tests for the PackCard.js component, specifically for
    elements the user would interact with

--- client/src/components/PackCard.js
+++ client/src/components/PackCard.js
@@ -3,22 +3,20 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import ThemeContext from '../contexts/themeContext';
 import { usePackDispatch } from '../contexts/packContext';
-import { slugify } from '../utilities/helpers';
-
 import { withRouter } from 'react-router';
 
 import { Title4, Caption3 } from './elements/Text';
 import LinkButton from './elements/LinkButton';
 
 PackCard.propTypes = {
+  _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  image: PropTypes.string,
 };
 
-function PackCard({ _id, name, image, match }) {
+function PackCard({ _id, name }) {
   const theme = useContext(ThemeContext);
-  const dispatch = usePackDispatch();
-  const setPack = () => dispatch({ type: 'SET_PACK_FILTER', id: _id, name: name });
+  // const dispatch = usePackDispatch();
+  // const setPack = () => dispatch({ type: 'SET_PACK_FILTER', id: _id, name: name });
 
   const PackCardWrapper = styled(LinkButton)`
     padding: .70rem;
@@ -40,21 +38,19 @@ function PackCard({ _id, name, image, match }) {
     }
   `;
 
-  // console.log(match);
-
   return (
     <PackCardWrapper
-        className="PackCard"
+        className='PackCard'
+        data-testid='card'
+        type='button'
         to={`/pack/${_id}`}
-        onClick={setPack}
       >
-        <Title4 className="PackCard__header">{name}</Title4>
-        <div className="PackCard__count-pill">
-          <Caption3 className="PackCard__count-text">CARDS</Caption3>  
+        <Title4 className='PackCard__header'>{name}</Title4>
+        <div className='PackCard__count-pill'>
+          <Caption3 className='PackCard__count-text'>CARDS</Caption3>  
         </div>
       </PackCardWrapper>
   );
 };
-
 
 export default withRouter(PackCard);
