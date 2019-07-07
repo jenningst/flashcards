d04956db1148db84aa10f0be5e367e098f423fb8
Troy Jennings <troy.c.jennings@gmail.com>
2019-07-07 08:21:39 +0200

    Remove dead import

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,6 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import Dashboard from './Dashboard';
 import ComposePack from './ComposePack';
 import PackContainer from '../containers/PackContainer';
-import PackHome from './PackHome';
 
 const apolloClient = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
