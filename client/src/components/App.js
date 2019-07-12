44bcf757a18f04a2cc56f3d0eae80ced55d618db
Troy Jennings <troy.c.jennings@gmail.com>
2019-07-12 11:47:05 +0200

    Refactor App to use AppProviders
    - Context tree was getting messy in App.js
    - Created AppProvider function to consolidate the top-level contexts for
    user and auth and place the contexts at the highest level in the tree
    - Refactored App.js to remove imports for the providers no longer in
    component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import { PackProvider } from '../contexts/pack-context';
-import { FirebaseProvider } from '../contexts/firebase-context';
-import { ApolloProvider } from 'react-apollo';
-
+import { useUser } from '../contexts/user-context';
 import { darkTheme, lightTheme } from '../themes/theme';
-import { ApolloClient, HttpLink } from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Dashboard from './Dashboard';
 import ComposePack from './ComposePack';
@@ -17,36 +12,28 @@ import Login from './Login';
 import SignUp from './SignUp';
 import ButtonViewer from './ButtonViewer';
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
-  cache: new InMemoryCache(),
-});
-
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const user = useUser();
 
   return (
-    <FirebaseProvider>
-      <ApolloProvider client={client}>
-        <PackProvider>
-          <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-            <AppWrapper className="App">
-              <Router>
-                <Switch>
-                  <Route path="/login" component={Login} />
-                  <Route path="/signup" component={SignUp} />
-                  <Route path="/forgot-password" component={SignUp} />
-                  <Route path="/home" exact component={Dashboard} />
-                  <Route path="/create-pack" component={ComposePack} />
-                  <Route path="/pack/:id" component={PackContainer} />
-                  <Route path='/btn-tester' component={ButtonViewer} />
-                </Switch>
-              </Router>
-            </AppWrapper>
-          </ThemeProvider>
-        </PackProvider>
-      </ApolloProvider>
-    </FirebaseProvider>
+    <PackProvider>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <AppWrapper className="App">
+          <Router>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/forgot-password" component={SignUp} />
+              <Route path="/home" exact component={Dashboard} />
+              <Route path="/create-pack" component={ComposePack} />
+              <Route path="/pack/:id" component={PackContainer} />
+              <Route path='/btn-tester' component={ButtonViewer} />
+            </Switch>
+          </Router>
+        </AppWrapper>
+      </ThemeProvider>
+    </PackProvider>
   );
 };
 
