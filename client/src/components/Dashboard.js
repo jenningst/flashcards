204d90a88fc46a820162cc7009a30276fa1deb78
Troy Jennings <troy.c.jennings@gmail.com>
2019-07-16 16:44:13 +0200

    Pre-responsive dash

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo';
 import { useSession } from '../contexts/user-context';
 
 import LinkButton from './elements/LinkButton';
-import { Title1, Title2, Body, Caption3 } from './elements/Text';
+import { Title1, Title3, Body, Caption3 } from './elements/Text';
 import { ReactComponent as Menu } from '../components/icons/svg/menu.svg';
 import { ReactComponent as Close } from '../components/icons/svg/error.svg';
 import PackCard from './PackCard';
@@ -36,68 +36,66 @@ function Dashboard() {
   const user = useSession();
 
   return (
-    <PageWrapper className="Dashboard">
-      <DashboardWrapper className="Dashboard__main">
-        <Header className="Dashboard__header">
-          <ButtonGroup className="button-label-group">
-            <MenuIcon
-              className="Dashboard__button-menu"
-              data-testid="button-menu"
-              onClick={toggleMenu}
-            />
-            <Caption3>Dashboard</Caption3>
-          </ButtonGroup>
-          <Avatar toggleMenu={toggleMenu}/>
-        </Header>
+    <DashboardWrapper className="Dashboard">
+      <Header className="Dashboard__header">
+        <ButtonGroup className="button-label-group">
+          <MenuIcon
+            className="Dashboard__button-menu"
+            data-testid="button-menu"
+            onClick={toggleMenu}
+          />
+          <Caption3>Dashboard</Caption3>
+        </ButtonGroup>
+        <Avatar toggleMenu={toggleMenu}/>
+      </Header>
 
-        <GreetingSection className="Dashboard__greeting-section">
-          <Caption3
-            className="greeting-date"
-            data-testid="greeting-date"
+      <GreetingSection className="Dashboard__greeting">
+        <Caption3
+          className="greeting-date"
+          data-testid="greeting-date"
+        >
+          {"October 19th"}
+        </Caption3>
+        <Title3 className="greeting-title">Ready to learn?</Title3>
+      </GreetingSection>
+
+      <StatsSection className="Dashboard__stats">
+        <Title3>Coming Soon</Title3>
+      </StatsSection>
+
+      <PackSection className="Dashboard__pack-section">
+        <Body>Your Packs</Body>
+        <PackList className="Dashboard__list">
+          <CreatePackButton
+            className="Dashboard__button-create-pack"
+            data-testid="create-pack-button"
+            type="button"
+            to={"/create-pack"}
           >
-            {"October 19th"}
-          </Caption3>
-          <Title2 className="greeting-title">Ready to learn?</Title2>
-        </GreetingSection>
+            <Title1>+</Title1>
+          </CreatePackButton>
+          
+          <UserPacks uid={user.uid}/>
+          
+        </PackList>
 
-        <PackSection className="Dashboard__pack-section">
-          <Body>Your Packs</Body>
-          <PackList className="Dashboard__list">
-            <CreatePackButton
-              className="Dashboard__button-create-pack"
-              data-testid="create-pack-button"
-              type="button"
-              to={"/create-pack"}
-            >
-              <Title1>+</Title1>
-            </CreatePackButton>
-            
-            <UserPacks uid={user.uid}/>
-            
-          </PackList>
-
-        </PackSection>
-      </DashboardWrapper>
-      {/* <Sidebar isOpen={showMenu} toggleOpen={toggleMenu} /> */}
-    </PageWrapper>
+      </PackSection>
+    </DashboardWrapper>
   );
 };
 
-const PageWrapper = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  min-height: 100%;
-`;
+/* Blocks */
 
-const DashboardWrapper = styled.section`
-  flex-grow: 2;
+const DashboardWrapper = styled.div`
   box-sizing: border-box;
+
   display: grid;
   grid-template-rows: repeat(2, auto) minmax(0, 1fr);
   height: 100%;
+  overflow: hidden;
+
   background: ${props => props.theme.color.main.offWhite};
   color: ${props => props.theme.color.fonts.charleston};
-  overflow: hidden;
 `;
 
 const Header = styled.header`
@@ -105,10 +103,44 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1.5rem;
+  padding: 1rem 1.5rem 1rem 1.5rem;
+
   background: ${props => props.theme.color.main.pureWhite};
+  box-shadow: 0px 10px 18px -8px rgba(0,0,0,0.05);
   color: ${props => props.theme.color.fonts.eerieBlack};
 `;
+
+const GreetingSection = styled.section`
+  grid-row: 2 / span 1;
+  padding: 2rem 1.5rem .50rem 1.5rem;
+
+  .greeting-date {
+    color: ${props => props.theme.color.fonts.grey};
+  }
+
+  .greeting-title {
+    font-weight: 500;
+    color: ${props => props.theme.color.fonts.charleston};
+  }
+`;
+
+const StatsSection = styled.section`
+  height: 5rem;
+`;
+
+const PackSection = styled.section`
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 1.5rem;
+  height: 100%;
+
+  p {
+    font-weight: 500;
+    margin-bottom: 2rem;
+  }
+`;
+
+/* Elements */
 
 const ButtonGroup = styled.div`
   display: flex;
@@ -133,32 +165,6 @@ const MenuIcon = styled(Menu)`
 const CloseIcon = styled(Close)`
   height: 1.5rem;
   width: 1.5rem;
-`;
-
-const GreetingSection = styled.section`
-  grid-row: 2 / span 1;
-  padding: 1rem 1.5rem 1rem 1.5rem;
-
-  .greeting-date {
-    color: ${props => props.theme.color.fonts.grey};
-  }
-
-  .greeting-title {
-    font-weight: 500;
-    color: ${props => props.theme.color.main.primary};
-  }
-`;
-
-const PackSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  padding: 1.5rem;
-  height: 100%;
-
-  p {
-    font-weight: 500;
-    margin-bottom: 1rem;
-  }
 `;
 
 const PackList = styled.div`
